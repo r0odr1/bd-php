3436e14a01bfb5053263da55b313bf95ffce8f10
Rodrigo <rhbon.08@gmail.com>
2024-04-16 18:17:24 -0500

    conectar php a base de datos mysql

--- includes/database.php
+++ includes/database.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "desde database.php";
+$db = mysqli_connect('localhost', 'root', 'roro123', 'appsalon');
+
+if(!$db) {
+  echo "Fallo de Conexion";
+  exit;
+} else {
+  echo "Conexion Exitosa";
+}
